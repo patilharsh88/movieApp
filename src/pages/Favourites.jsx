8a72691984def62ab9533f9a1712809323df4a0d
patilharsh88 <patilharshvardhank27502@gmail.com>
2026-04-28 13:25:03 +0530

    updated fav

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -5,9 +5,6 @@ function Favourites(){
         <>
             <div className="favourites-empty" >
 
-                <h2>No Fav movies yet</h2>
-                <p>start  adding your fav movies and they will show here </p>
-
             </div>
         </>
     )    
